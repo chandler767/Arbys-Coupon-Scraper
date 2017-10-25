184e922ef4e24e785d8805e6ca4045e6a389a268
Chandler Mayo <chandler@chandlermayo.com>
2017-10-25 11:20:55 -0400

    Improved comments.

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func init() {
 	flag.IntVar(&max, "concurrent", 45, "Maximum concurrent requests")                                                    // Prevents "Too many open files" error and timeouts.
 	flag.IntVar(&mailingid, "mailingid", 27917361605, "Mailing ID to start on")                                           // Get a recent MailingID from an Arby's promo email by clicking to view print version and getting the query value for 'MailingID' from the url. URL Example from 10/25/2017: http://arbys.fbmta.com/members/ViewMailing.aspx?MailingID=27917361605
 	flag.StringVar(&scrapeurl, "scrapeurl", "http://arbys.fbmta.com/members/ViewMailing.aspx?MailingID=", "URL to scape") // This was found by viewing a printable version of an Arby's promo email and removing the extra query values from the URL.
-	flag.BoolVar(&formatcoupon, "formatcoupon", true, "Remove the store text and expiration dates.")                      // Remove the "Offer valid only at:" text and expiration dates.
+	flag.BoolVar(&formatcoupon, "formatcoupon", true, "Remove the 'Offer valid only at:' text and expiration dates.")     // Remove the "Offer valid only at:" text and expiration dates.
 }
 
 type Response struct {
